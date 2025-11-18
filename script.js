6222aecc9707887180d80620904601df379e56e8
Zachary Martinez <zach.martinez@protonmail.com>
2025-11-17 17:36:34 -0800

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function getRandomInt(max) {
-    return Math.floor((Math.random * max));
-}
-
 function getComputerChoice () {
     /*
         CREATE variable n
@@ -18,4 +14,29 @@ function getComputerChoice () {
     return  n == 1 ? "Rock" 
           : n == 2 ? "Paper"
           : "Scissors";
+}
+
+function getHumanChoice () {
+    /* 
+        CREATE variable input
+
+        PROMPT user with message: 
+            "Enter your choice, 1: Rock, 2: Paper, 3: Scissors"
+        
+        STORE value from prompt in input
+
+        CONVERT input to a number
+
+        IF input is equal to 1 then return rock
+        ELSEIF input is equal to 2 then return paper
+        ELSE return scissors
+    */
+
+    let promptMessage = `Enter your choice:\n 1 for "Rock", 2 for "Paper", and 3 for "Scissors".`;
+    let inputStr = prompt(promptMessage);
+    let inputInt = +inputStr;
+
+    return inputInt == 1 ? "Rock" 
+         : inputInt == 2 ? "Paper"
+         : "Scissors";
 }
