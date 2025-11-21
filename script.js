f825c70d68f5835385f433a241f1cb0ed4f00f5a
Zachary Martinez <zach.martinez@protonmail.com>
2025-11-21 14:44:12 -0800

    Add getRandomKey utility function

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
-let humanScore = 0;
-let computerScore = 0;
-const values = {
-    "rock": 1,
-    "paper": 2,
-    "scissors": 3
+const values = {"rock": 1,"paper": 2,"scissors": 3};
+
+function getRandomKey(obj) {
+    const keys = Object.keys(obj);
+    const n = Math.floor(Math.random() * keys.length);
+
+    return keys[n];
 }
 
-function getComputerChoice () {
-    /*
-        CREATE variable n
+function getComputerChoice() {
+    // SET variable n to a random number from 0 to 2
+    // SET variable keys to an array of the keys from the values object
+    // RETURN the key with the index of n
 
-        CREATE random number from 1-3 and store that value in n
+    const n = Math.floor(Math.random() * 3);
+    const keys = Object.keys(values);
 
-        IF n is equal to 1 then choose rock
-        ELSIF  n is equal to 2 then choose paper
-        ELSE choose scissors
-    */
-
-    let n = Math.floor(Math.random() * 3);
-
-    return  n == 1 ? "rock" 
-          : n == 2 ? "paper"
-          : "scissors";
+    return keys[n];
 }
+    
+function getHumanChoice() {
+    // INIT inputIsValid to false
+    // INIT inputString 
 
-function getHumanChoice () {
-    /* 
-        INIT inputIsValid to false
-        INIT inputString 
-        
-        WHILE inputIsValid is false
-            IF inputString matches pattern 
-                Set inputIsValid to true
-            ELSE
-                Get user input and assign the value to inputString
-            ENDIF
-        ENDLOOP
+    // WHILE inputIsValid is false
+    // IF inputString matches pattern 
+    // Set inputIsValid to true
+    // ELSE
+    // Get user input and assign the value to inputString
+    // ENDIF
+    // ENDLOOP
 
-        RETURN inputString converted to lowercase
-    */
+    // RETURN inputString converted to lowercase
 
-    let inputIsValid = false; 
+    let inputIsValid = false;
     let inputString = prompt("Enter your choice:");
     let pattern = /\b(rock|paper|scissors)\b/i; // I used chatgpt for this
 
@@ -55,47 +47,75 @@ function getHumanChoice () {
     return inputString.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    /*
-        SET variable humanValue to the corresponding value from the values object
-        SET variable computerValue to the corresponding value from the values object
-        SET variable outcomeValue to humanValue - computerValue % 3
-
-        IF the outcomValue is equal to 1
-            increment humanScore by 1
-            PRINT victory message string template
-        ELSIF the outcomeValue is equal to 2 or -1
-            increment computerScore by 1
-            PRINT defeat message string template
-        ELSE
-            PRINT tie message string template
-
-        PRINT scoreboard string template
-    */
-    const humanValue = values[humanChoice];
-    const computerValue = values[computerChoice];
-    const outcomeValue = humanValue - computerValue % 3;
-    let isWinner = false;
-
-    if ( outcomeValue == 1 ) {
-        humanScore++;
-        isWinner = true;
-        console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
-    } else if ( outcomeValue == 2 || outcomeValue == -1 ) {
-        computerScore++;
-        isWinner = true;
-        console.log(`You lost. ${computerChoice} beats ${humanChoice}.`);
-    } else {
-        isWinner = false;
-        console.log(`It's a tie. Let's try again!`);
-    }
-
-    console.log(`The score is, You: ${humanScore}, Computer: ${computerScore}`);
-    return isWinner;
-}
 
 function playGame() {
-    /*
+    // SET variable maxRounds to 5
+    // SET variable roundsCounter to 0
+    // SET variable isWinner to false
+    
+    // WHILE roundsCounter is less than maxRounds
+    // IF isWinner
+    // increment roundsCounter by 1
+    
+    // SET variable isWinner to the output of running playRound
+    // ENDLOOP 
 
-    */
+    function playRound(humanChoice, computerChoice) {
+        
+        // SET variable humanValue to the corresponding value from the values object
+        // SET variable computerValue to the corresponding value from the values object
+        // SET variable outcomeValue to humanValue - computerValue % 3
+        
+        // IF the outcomValue is equal to 1
+        //     increment humanScore by 1
+        //     PRINT victory message string template
+        // ELSIF the outcomeValue is equal to 2 or - 1
+        //     increment computerScore by 1
+        //     PRINT defeat message string template
+        // ELSE
+        //     PRINT tie message string template
+        
+        // PRINT scoreboard string template
+        
+        const humanValue = values[humanChoice];
+        const computerValue = values[computerChoice];
+        const outcomeValue = humanValue - computerValue % 3;
+        let isWinner = false;
+        
+        if (outcomeValue == 1) {
+            humanScore++;
+            isWinner = true;
+            console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+        } else if (outcomeValue == 2 || outcomeValue == -1) {
+            computerScore++;
+            isWinner = true;
+            console.log(`You lost. ${computerChoice} beats ${humanChoice}.`);
+        } else {
+            isWinner = false;
+            console.log(`It's a tie. Let's try again!`);
+        }
+        
+        console.log(`The score is, You: ${humanScore}, Computer: ${computerScore}`);
+        return isWinner;
+    }
+    
+    const maxRounds = 5;
+    let roundsCounter = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+    let isWinner = false;
+    
+    while (roundsCounter < maxRounds) {
+        isWinner = playRound(getHumanChoice(), getComputerChoice());
+
+        if (isWinner) {
+            roundsCounter++;
+        }
+    }
+    
+    if (humanScore > computerScore) {
+        console.log(`Yay! You won the game!`);
+    } else {
+        console.log(`Sorry, you lost the game.`);
+    }
 }
