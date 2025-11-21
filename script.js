41b91648ea63e5119422f3dbbf5fd5972a0b49e5
Zachary Martinez <zach.martinez@protonmail.com>
2025-11-21 10:27:15 -0800

    Add first iteration of playRound function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+const values = {
+    "rock": 1,
+    "paper": 2,
+    "scissors": 3
+}
 
 function getComputerChoice () {
     /*
@@ -48,4 +53,38 @@ function getHumanChoice () {
     }
 
     return inputString.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    /*
+        SET variable humanValue to the corresponding value from the values object
+        SET variable computerValue to the corresponding value from the values object
+        SET variable outcomeValue to humanValue - computerValue % 3
+
+        IF the outcomValue is equal to 1
+            increment humanScore by 1
+            PRINT victory message string template
+        ELSIF the outcomeValue is equal to 2 or -1
+            increment computerScore by 1
+            PRINT defeat message string template
+        ELSE
+            PRINT tie message string template
+
+        PRINT scoreboard string template
+    */
+    const humanValue = values[humanChoice];
+    const computerValue = values[computerChoice];
+    const outcomeValue = humanValue - computerValue % 3;
+
+    if (outcomeValue == 1) {
+        humanScore++;
+        console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+    } else if (outcomeValue == 2 || -1) {
+        computerScore++;
+        console.log(`You lost. ${computerChoice} beats ${humanChoice}.`)
+    } else {
+        console.log(`It's a tie. Let's try again!`);
+    }
+
+    console.log(`The score is, You: ${humanScore}, Computer: ${computerScore}`);
 }
