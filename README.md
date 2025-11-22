02932b0372ee187061a99626042583e991701985
Zachary Martinez <zach.martinez@protonmail.com>
2025-11-22 12:38:00 -0800

    Make game logic work, update README

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ I included some features beyond the strict scope of the assignment. They include
 ### Input Handling
 The `getHumanInput` function takes a regular expression as a parameter that it use to check user input to make sure it matches. The regexp is generated dynamically from an array of string representing the items in play.
 
-### Support for Extra 'Weapons'
-The method for determining which item wins is based on an array of strings with an arbitrary length and some funky math with the `%` operator, so in theory you could have extra items.
+### Mathematical Game Modeling
+The method for determining which item wins is based on an array of strings with an arbitrary length and some funky math with the `%` operator, so in theory you could have extra items, like in "Rock-Paper-Scissors-Lizard-Spock". I haven't been able to get this to work in implementation, I don't understand the math well enough.
 
-I had help from [this article](https://medium.com/@jp.mfichtl/rock-paper-scissors-lizard-spock-or-why-math-is-awesome-for-coding-405dabe30f4) by Juan F. on Medium on modular arithmetic.
+I had help from [this article](https://therenegadecoder.com/code/rock-paper-scissors-using-modular-arithmetic/) by Jeremy Grifski on modular arithmetic.
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function getComputerChoice(array) {
-    const randomIndex = Math.floor(Math.random() * array.length);    
+    const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 }
 
 function getHumanChoice(pattern) {
     let inputValid = false;
-    let inputString = prompt('Enter your choice: '); 
-
+    let inputString = prompt('Enter your choice: ');
+    
     while (inputValid == false) {
-        if ( pattern.test(inputString) ) {
+        if (pattern.test(inputString)) {
             inputValid = true;
             return inputString.toLowerCase();
         } else {
@@ -17,46 +17,43 @@ function getHumanChoice(pattern) {
     }
 }
 
-function playGame() {
-    const weapons = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+function playGame(weapons, maxRounds) {
     const weaponCount = weapons.length;
-    const pattern = new RegExp(`\\b(${weapons.join('|')})\\b`, 'i');
-    const maxRounds = 5;
+    const pattern = new RegExp(`\\b(${weapons.join('|')})\\b`, `i`);
     let roundsCounter = 0;
     let humanScore = 0;
     let computerScore = 0;
-    
-    function playRound(humanChoice, computerChoice) {        
-        const humanValue = weapons.indexOf(humanChoice);
-        const computerValue = weapons.indexOf(computerChoice);
-        const outcomeValue = Math.abs(humanValue - computerValue);
-        
+
+    function playRound(humanChoice, computerChoice) {
+        const humanChoiceInt = weapons.indexOf(humanChoice);
+        const computerChoiceInt = weapons.indexOf(computerChoice);
+        // console.log(humanChoiceInt, computerChoiceInt);
+
         let isWinner = false;
-        if (outcomeValue % weaponCount == 0) {
-            isWinner = false;
-            console.log(`It's a tie. Let's try again!`);
-        } else if ((outcomeValue % weaponCount) % 2 === 0) {
-            humanScore++;
-            isWinner = true;
-            console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
-        } else {
+        if ( humanChoiceInt === computerChoiceInt ) {
+            console.log(`It's a tie! You both chose ${humanChoice}.`);
+        } else if ( (humanChoiceInt + 1) % weaponCount === computerChoiceInt ) {
             computerScore++;
             isWinner = true;
-            console.log(`You lost. ${computerChoice} beats ${humanChoice}.`);
-        }   
-        
-        console.log(`The score is, You: ${humanScore}, Computer: ${computerScore}`);
-        return isWinner; // This flag gets used to increment the rounds counter
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        } else {
+            humanScore++;
+            isWinner = true;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        }
+
+        console.log(`The score is; You: ${humanScore}, Computer: ${computerScore}`);
+        return isWinner;
     }
-    
+
     let isWinner = false;
     while (roundsCounter < maxRounds) {
-        isWinner = playRound(getHumanChoice(pattern), getComputerChoice(weapons));
+        isWinner = playRound( getHumanChoice(pattern), getComputerChoice(weapons) );
         
         let leadingScore = Math.max(humanScore, computerScore);
         if (isWinner) {
             roundsCounter++;
-
+            
             if (leadingScore / maxRounds > 0.5) { // End game early if lead cannot be recovered
                 break;
             }
@@ -69,3 +66,5 @@ function playGame() {
         console.log(`Sorry, you lost the game.`);
     }
 }
+
+playGame(['rock', 'paper', 'scissors'], 5);
